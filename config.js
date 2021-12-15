d4480472712448f20e9f2f5c83c6dbc8003966d0
beuqk <96179328+beuqk@users.noreply.github.com>
2021-12-15 20:27:09 +0700

    to mom
    love you <3

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Hey mom🖐',
+    introDesc: `I just have a thing to say`,
+    btnIntro: 'ok',
+    title: 'I m broke 😅',
+    desc: 'Please buff mana for me 😅😅😅😅 ',
+    btnYes: 'Yes , for sure 💖',
+    btnNo: '💔 Hell no 💔 ',
+    question:'Yesssss !!! Love you mom 😊😊',
+    reasonPlaceholder: 'bla bla',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'I love you too babe <3 <3',
+    mess: 'I knew it ! Love you so much mom 💖. Love you 3 thousand 💖',
+    messDesc: '💬💬How you think about this web I build up in 2 days💬💬',
+    btnAccept: 'Text me',
+    messLink: 'https://www.facebook.com/beucuteovcl/'
+}
